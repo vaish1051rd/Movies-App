19abd9cd5be2f06a61582c2c626a03dac74ca0d3
Vaishnavi Gupta <vaish101navi@gmail.com>
2023-09-17 17:57:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Banner from './Components/Banner';
+import Movies from './Components/Movies';
+import Favourite from './Components/Favourite';
+// import {BrowserRouter as Router,Switch,Route, BrowserRouter} from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path='/' exact render={(props)=>(
+          <>
+            <Banner {...props}/>
+            <Movies {...props}/>
+          </>
+        )}/>
+        <Route path='/favourites' component={Favourite} />
+      </Switch>
+     
+    </Router>
   );
 }
 
